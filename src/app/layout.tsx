0ec8cc58678b8ba563f3b4ea00e4038851563ed1
Muhammad Hamza Khan <hamza.khansahab@gmail.com>
2025-07-12 14:30:46 +0500

    Integrate Dify chatbot into layout
    - Added custom styles for the Dify chatbot button and window dimensions.
    - Configured Dify chatbot settings with user and system variables.
    - Included Dify chatbot script for embedding functionality in the application.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,10 +24,47 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
+              #dify-chatbot-bubble-button {
+                background-color: #1C64F2 !important;
+              }
+              #dify-chatbot-bubble-window {
+                width: 24rem !important;
+                height: 40rem !important;
+              }
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.difyChatbotConfig = {
+                token: 'qfojOzOqEN3C54Dz',
+                systemVariables: {
+                  // user_id: 'YOU CAN DEFINE USER ID HERE',
+                  // conversation_id: 'YOU CAN DEFINE CONVERSATION ID HERE, IT MUST BE A VALID UUID',
+                },
+                userVariables: {
+                  // avatar_url: 'YOU CAN DEFINE USER AVATAR URL HERE',
+                  // name: 'YOU CAN DEFINE USER NAME HERE',
+                },
+              }
+            `,
+          }}
+        />
+        <script
+          src="https://udify.app/embed.min.js"
+          id="qfojOzOqEN3C54Dz"
+          defer
+        />
       </body>
     </html>
   );
